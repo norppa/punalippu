12f22d9813430846abe7070d948fe7eecfff34d1
norppa <norppa@mailinator.com>
2018-09-04 15:26:41 +0300

    heroku deployment

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,13 +5,16 @@ const loginRouter = require('express').Router()
 
 loginRouter.post('/', (request, response) => {
     const pwdHash = process.env.ADMIN_HASH
+    console.log("pwdHash found", pwdHash)
 
     return bcrypt.compare(request.body.password, pwdHash)
         .then(passwordCorrect => {
             if (passwordCorrect) {
+                console.log(passwordCorrect)
                 const token = jwt.sign({user: 'admin'}, process.env.SECRET)
                 return response.status(200).send({token})
             } else {
+                console.log(passwordCorrect)
                 return response.status(401).send({error: 'incorrect password'})
             }
         })
